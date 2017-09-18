7acdcc9ccd44119b1d13fe13b29d162dd804ebc5
Christian <xianlanoue@gmail.com>
2017-09-18 09:43:09 -0400

    pedals_controller

--- app/controllers/pedals_controller.rb
+++ app/controllers/pedals_controller.rb
@@ -1,13 +1,40 @@
 class PedalsController < ApplicationController
-  def index
-  end
 
-  def edit
+  def index
+    @pedals = Pedal.all
   end
 
   def new
+    @pedal = Pedal.new
+  end
+
+  def create
+    @pedal = Pedal.create!(pedal_params)
+    redirect_to pedal_path(@pedal)
   end
 
   def show
+    @pedal = Pedal.find(params[:id])
   end
-end
+
+  def edit
+    @pedal = Pedal.find(params[:id])
+  end
+
+  def update
+    @pedal = Pedal.find(params[:id])
+    @pedal.update(pedal_params)
+    redirect_to pedal_path(@pedal)
+  end
+
+  def destroy
+    @pedal = Pedal.find(params[:id])
+    @pedal.destroy
+    redirect_to pedals_path
+  end
+
+  private
+    def pedal_params
+      params.require(:pedal).permit(:name, :photo_url)
+    end
+  end
